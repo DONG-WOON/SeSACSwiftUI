5c90ffce4723e367d6c39872ba91efaa1b9ab61c
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-11-13 09:42:48 +0900

    Initial Commit

---
+++ SeSACSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SeSACSwiftUI
+//
+//  Created by 서동운 on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeSACSwiftUI/SeSACSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSwiftUIApp.swift
+//  SeSACSwiftUI
+//
+//  Created by 서동운 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
